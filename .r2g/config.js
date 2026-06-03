550246b62877fd5b89c1611a7400ebf599a64944
Alexander Mills <alexander.d.mills@gmail.com>
2026-06-02 19:39:30 -0500

    expand r2g phase coverage

--- .r2g/config.js
+++ .r2g/config.js
@@ -1,17 +1,18 @@
-// note: only include dependencies in this file, which are in your project's package.json file
 const path = require('path');
 
-if (!path.isAbsolute(process.env.MY_DOCKER_R2G_SEARCH_ROOT || '')) {
-  throw new Error('Please set the env var "MY_DOCKER_R2G_SEARCH_ROOT" to an absolute path.');
+const searchRoot = path.resolve(
+  process.env.R2G_SEARCH_ROOT ||
+  process.env.MY_DOCKER_R2G_SEARCH_ROOT ||
+  process.env.HOME ||
+  ''
+);
+
+if (!path.isAbsolute(searchRoot)) {
+  throw new Error('Please set R2G_SEARCH_ROOT or MY_DOCKER_R2G_SEARCH_ROOT to an absolute path.');
 }
 
 exports.default = {
-
-  searchRoot: path.resolve(process.env.MY_DOCKER_R2G_SEARCH_ROOT),
+  searchRoot,
   tests: '',
-  packages: {
-
-
-  }
-
+  packages: {}
 };
